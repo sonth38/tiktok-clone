a4433e4a604ad6a6bd2f0e1e4bd5e625f7098fb7
Son Ta <sontahvac@gmail.com>
2024-07-17 23:09:00 +0700

    setup and config react-router-dom

---
+++ src/Layouts/HeaderOnly/index.js
@@ -0,0 +1,14 @@
+import Header from '~/Layouts/components/Header';
+
+function HeaderOnly({ children }) {
+    return (
+        <div className="">
+            <Header />
+            <div className="container">
+                <div className="content">{children}</div>
+            </div>
+        </div>
+    );
+}
+
+export default HeaderOnly;
